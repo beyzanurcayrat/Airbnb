d46e7830527cf15e5e17bc8e1dbd9a29fab720bd
beyzacayrat <beyzacayrat@gmail.com>
2024-01-27 15:08:55 +0300

    Initial Commit

---
+++ AirbnbApp/AirbnbAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbAppApp.swift
+//  AirbnbApp
+//
+//  Created by beyzanur cayrat on 27.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
