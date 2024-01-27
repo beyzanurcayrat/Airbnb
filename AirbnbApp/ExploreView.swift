c60bceb5e9ee4c61965767768238dcd0710eb4b3
beyzacayrat <beyzacayrat@gmail.com>
2024-01-27 15:24:39 +0300

    Created Airbnb project

---
+++ AirbnbApp/ExploreView.swift
@@ -0,0 +1,22 @@
+//
+//  ExploreView.swift
+//  AirbnbApp
+//
+//  Created by beyzanur cayrat on 27.01.2024.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView{
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
