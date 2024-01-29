b876a7eff60e73002db102c90c55a8e17c8136b6
beyzacayrat <beyzacayrat@gmail.com>
2024-01-29 15:07:29 +0300

    More updates

--- AirbnbApp/ListingItemView.swift
+++ AirbnbApp/ListingItemView.swift
@@ -4,14 +4,74 @@
 //
 //  Created by beyzanur cayrat on 27.01.2024.
 //
-
+ 
 import SwiftUI
 
-struct ListingView: View {
+struct ListingItemView: View {
+    
+    var images = [
+    "listing-1",
+    "listing-2",
+    "listing-3",
+    "listing-4",
+    
+    ]
+    
     var body: some View {
 
-        
+        VStack(spacing: 8){
+            //images
+            
+            ListingImageCarouselView()
+                .frame(height: 320)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            
 
+            
+            HStack(alignment: .top) {
+                //details
+                VStack(alignment: .leading){
+                    Text("Miami, Florida")
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.black)
+                    
+                    Text("12 mil away")
+                        .foregroundStyle(.gray)
+                    
+                    Text("Nov 2-10")
+                        .foregroundStyle(.gray)
+                    
+                    HStack(spacing: 4){
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        Text("night")
+                        
+                    }
+                    .foregroundStyle(.black)
+
+  
+                    
+                    
+                }
+                
+                Spacer()
+                
+                // rating
+                HStack(spacing: 2){
+                    Image(systemName: "star.fill")
+                    
+                    Text("4.60")
+
+                }
+                .foregroundStyle(.black)
+
+                
+            }
+            .font(.footnote)
+
+
+        }
+        
     }
 }
 
