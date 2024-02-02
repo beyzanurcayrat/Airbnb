828f33b859f4d51c880178fbc37f0f4735a3894a
beyzanurcayrat <beyzacayrat@gmail.com>
2024-02-02 23:20:39 +0300

    Added updates

--- AirbnbApp/Extensions/Regions.swift
+++ AirbnbApp/Extensions/Regions.swift
@@ -11,6 +11,6 @@ import CoreLocation
 extension CLLocationCoordinate2D {
     static var losAngeles = CLLocationCoordinate2D(latitude: 34.0549, longitude: -118.2426)
     static var miami = CLLocationCoordinate2D(latitude: 25.7602, longitude: -80.1959)
-    static var kirikkale = CLLocationCoordinate2D(latitude: 25.7602, longitude: -80.1959)
+    static var kirikkale1 = CLLocationCoordinate2D(latitude: 25.7602, longitude: -80.1959)
 
 }
